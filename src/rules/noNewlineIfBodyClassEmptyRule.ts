9166e190d35f5df2010ae226af09b55c08210181
Milo W. (Aka. Mr. Seal/Vyrek) <vyrekxd@gmail.com>
2025-03-17 09:26:06 -0600

    Update noNewlineIfBodyClassEmptyRule.ts

--- src/rules/noNewlineIfBodyClassEmptyRule.ts
+++ src/rules/noNewlineIfBodyClassEmptyRule.ts
@@ -30,7 +30,7 @@ export default function create(createRule: ReturnType<typeof ESLintUtils.RuleCre
                     'No newline if body class is empty.',
             },
             messages: {
-                nosecomoponerle: 'XDXDDXXD??',
+                noNewLine: 'Empty body class should not have a newline.',
             },
             type: 'suggestion',
             schema: [],
